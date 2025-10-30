ae07ba52d532c274fbb5ce338a9ac40722a68124
a.savanovich <asavanster@gmail.com>
2025-10-30 18:24:17 +0300

    less logs

--- src/js/starter.js
+++ src/js/starter.js
@@ -6,7 +6,9 @@ import {writeUrlWithTimeout} from "./nfcutils.js";
 export default function starter(window, document) {
     const changed = parseSettings(window.location.search, settings);
     const mainLogger = loggerFunc(document, settings);
-    mainLogger.log("Extra options " + JSON.stringify(changed));
+    if (changed) {
+        mainLogger.log("Extra options " + JSON.stringify(changed));
+    }
 
     const writeBtn = document.querySelector(".js-write-nfc");
     const readBtn = document.querySelector(".js-read-nfc");
@@ -15,22 +17,6 @@ export default function starter(window, document) {
     const input = document.querySelector("#dataInput");
     const inputSsid = document.querySelector("#ssidInput");
     const passwordInput = document.querySelector("#passwordInput");
-    const hotel = "SSID:\tMAY BEACH HOTEL T3\n" +
-        "Protocol:\tWi-Fi 5 (802.11ac)\n" +
-        "Security type:\tWPA2-Personal\n" +
-        "Manufacturer:\tIntel Corporation\n" +
-        "Description:\tIntel(R) Wireless-AC 9560 160MHz #3\n" +
-        "Driver version:\t23.110.0.5\n" +
-        "Network band (channel):\t5 GHz (149)\n" +
-        "Aggregated link speed (Receive/Transmit):\t351/260 (Mbps)\n" +
-        "Link-local IPv6 address:\tfe80::e6e1:7568:9df5:8857%22\n" +
-        "IPv4 address:\t192.168.110.48\n" +
-        "IPv4 default gateway:\t192.168.110.1\n" +
-        "IPv4 DNS servers:\t192.168.110.1 (Unencrypted)\n" +
-        "Physical address (MAC):\tCC:D9:AC:B1:62:19\n";
-    const ssidHolel = "MAY BEACH HOTEL T3";
-    const pass = "12345678";
-    console.log(hotel, ssidHolel, pass);
     try {
         cleanBtn.addEventListener("click", () => {
             logElem.innerHTML = "";
--- tests/nfc.test.js
+++ tests/nfc.test.js
@@ -173,3 +173,22 @@ test("nfc simple 3", () => {
     assert.equal(pass, decoded.networkKey);
     assert.deepEqual(ndefWifiSimple.authTypes.WPA2_PSK, decoded.authType);
 });
+
+test("print hotel", () => {
+    const hotel = "SSID:\tMAY BEACH HOTEL T3\n" +
+        "Protocol:\tWi-Fi 5 (802.11ac)\n" +
+        "Security type:\tWPA2-Personal\n" +
+        "Manufacturer:\tIntel Corporation\n" +
+        "Description:\tIntel(R) Wireless-AC 9560 160MHz #3\n" +
+        "Driver version:\t23.110.0.5\n" +
+        "Network band (channel):\t5 GHz (149)\n" +
+        "Aggregated link speed (Receive/Transmit):\t351/260 (Mbps)\n" +
+        "Link-local IPv6 address:\tfe80::e6e1:7568:9df5:8857%22\n" +
+        "IPv4 address:\t192.168.110.48\n" +
+        "IPv4 default gateway:\t192.168.110.1\n" +
+        "IPv4 DNS servers:\t192.168.110.1 (Unencrypted)\n" +
+        "Physical address (MAC):\tCC:D9:AC:B1:62:19\n";
+    const ssidHolel = "MAY BEACH HOTEL T3";
+    const pass = "12345678";
+    console.log(hotel, ssidHolel, pass);
+});
